3a9de0635f408455c14b6d62ce63253eb7d993f5
Haysam <haysam2002@gmail.com>
2020-03-02 22:28:54 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
