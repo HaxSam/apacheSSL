3a1569eea873a1d153fe5fa108a9248526e61b51
Haysam <haysam2002@gmail.com>
2020-03-02 22:11:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 apt update
-apt install docker
+apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-commo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker
 docker build --no-cache -t aufgabe .
 docker run -d --name server --hostname localhost -p 80:80 -p 443:443 --rm -it aufgabe
 docker exec server service apache2 start
