8cdda2e252888da65e9b804dd15a7b621066376d
Haysam <haysam2002@gmail.com>
2020-03-02 22:22:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 apt update
-apt install apt-transport-https ca-certificates curl gnupg-agent
+apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker
 docker build --no-cache -t aufgabe .
