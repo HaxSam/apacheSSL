64cfce68569a7489315e1267f7e6b2efdfa6efad
Haysam <haysam2002@gmail.com>
2020-03-02 22:14:48 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-commo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt install apt-transport-https ca-certificates curl gnupg-agent
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
