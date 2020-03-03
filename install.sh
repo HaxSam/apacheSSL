a601b1fe34d5ee37d677d45599a4401bffc12a60
Haysam <haysam2002@gmail.com>
2020-03-03 13:04:50 +0000

    Update install.sh | changed Dockerfile

--- install.sh
+++ install.sh
@@ -22,5 +22,4 @@ apt install docker-ce docker-ce-cli containerd.io docker
 
 docker build --no-cache -t aufgabe .
 docker run -d --name server --hostname localhost -p 80:80 -p 443:443 --rm -it aufgabe
-docker exec server service apache2 start
 
